150bf758de99c2871494b74c4911575e65d8e74a
Aarnav Pant <aarnav.pant@sap.com>
2023-06-16 10:21:19 +0530

    test specs

--- students/student.go
+++ students/student.go
@@ -1,11 +1,36 @@
 package students
 
+import (
+	"strings"
+)
+
 type Student struct {
 	Name   string
 	RollNo int
 	// subjects []string
 }
 
-func (s Student) getName() string {
+func (s Student) GetName() string {
 	return s.Name
+}
+
+func (s Student) GetRoll() int {
+	return s.RollNo
+}
+
+func (s Student) GetFirstName() string {
+	names := strings.Split(s.Name, " ")
+	return names[0]
+}
+
+func (s Student) GetLastname() string {
+	names := strings.Split(s.Name, " ")
+	if len(names) > 2 {
+		name_str := ""
+		for _, name := range names[1:] {
+			name_str += name
+		}
+		return name_str
+	}
+	return names[len(names)-1]
 }
